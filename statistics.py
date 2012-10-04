4eb26d7f445185c44bbd59a89802bb25b6e36b15
Leandro Lovisolo <leandro@leandro.me>
2012-10-03 22:46:52 -0300

    El script ahora genera CSV a stdout con todo lo necesario para el TP

--- statistics.py
+++ statistics.py
@@ -12,7 +12,7 @@ def mean_word_length(ipu_filename):
     get_words      = lambda x: string.join(x.strip().split(" ")[2:])
     is_not_silence = lambda x: x != "#"
     to_array       = lambda x, y: x + y.split(" ")
-    to_word_length = lambda x: len(x)
+    to_word_length = lambda x: float(len(x))
 
     words   = map(get_words, lines)
     words   = filter(is_not_silence, words)
@@ -72,18 +72,64 @@ def analyze_directory(dirname, function, key_suffix, corpus_data):
         new_key        = "task_" + fn_info["task"] + "_" + key_suffix
         datum[new_key] = function(dirname + "/" + fn)
 
-def main():
-    # if len(sys.argv) < 2:
-    #     print "Usage: " + sys.argv[0] + " <operation>"
-    #     print "  where <operation> is one of:"
-    #     print "    foo: documentation"
-    #     print "    bar: documentation"
-    #     return
+def mean_or_none(x, y):
+    if x != None and y != None:
+        return (x + y) / 2
+    elif x != None and y == None:
+        return x
+    elif x == None and y != None:
+        return y
+    else:
+        return None
 
+def main():
+    if len(sys.argv) != 2 or not (sys.argv[1] in ["age-vs-mean-word-length",
+                                                  "gender-vs-mean-f0",
+                                                  "scripted-vs-natural-mean-f0"]):
+        print "Usage: " + sys.argv[0] + " <operation>"
+        print "  where <operation> is one of:"
+        print "    age-vs-mean-word-length:     produces a list of (age, mean word length) pairs."
+        print "    gender-vs-mean-f0:           produces a list of (gender, mean f0) pairs."
+        print "    scripted-vs-natural-mean-f0: produces a list of (scripted f0, natural f0) pairs."
+        return
+    
     corpus_data = load_corpus_data()
     analyze_directory("corpus/ipus", mean_word_length, "mean_word_length", corpus_data)
     analyze_directory("corpus/csvs", mean_f0, "mean_f0", corpus_data)
 
-    print corpus_data
+    operation = sys.argv[1]
+
+    if operation == "age-vs-mean-word-length":
+        print "Age,Mean Word Length"
+        for datum in corpus_data:
+            mean = mean_or_none(datum.get("task_1_mean_word_length"),
+                                datum.get("task_2_mean_word_length"))
+            if mean != None:
+                print datum["age"] + "," + str(mean)
+        return
+
+    if operation == "gender-vs-mean-f0":
+        print '"Gender (0 = male, 1 = female)","Mean F0"'
+        for datum in corpus_data:
+            mean = mean_or_none(datum.get("task_1_mean_f0"),
+                                datum.get("task_2_mean_f0"))
+            if mean != None:
+                print ("0" if datum["gender"] == "m" else "1") + "," + str(mean)
+        return
+
+    if operation == "scripted-vs-natural-mean-f0":
+        print '"Scripted F0","Natural F0"'
+        for datum in corpus_data:
+            scripted = datum.get("task_1_mean_f0")
+            natural  = datum.get("task_2_mean_f0")
+            if scripted == None and natural == None:
+                continue
+            else:
+                def none_as_blank(x): return "" if x == None else str(x)
+                print none_as_blank(scripted) + "," + none_as_blank(natural)
+        return    
+
+    if operation == "natural-vs-scripted-mean-f0":
+        pass
 
 if __name__ == '__main__': main()
